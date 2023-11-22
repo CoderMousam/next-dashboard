0433e68e7abf40640811b547d77094a446734cdb
CoderMousam <mousam.officialuse@gmail.com>
2023-11-22 15:40:24 +0530

    added alt in invoices table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,7 +27,8 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
-                      alt=""                        src={invoice.image_url}
+                      alt="Invoice image alt"                        
+                      src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
